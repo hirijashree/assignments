6ed04fb9bae45cfd801db7c59e5ac806465c0350
hirijashree <108250556+hirijashree@users.noreply.github.com>
2022-07-20 08:44:24 +0530

    Add files via upload

---
+++ sys20-07-2022/inc/multi.h
@@ -0,0 +1,12 @@
+nclude <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stdio.h>
+#define PATH "/home/hirijashree/hiri/sys20-07-2022/data"
+
+using namespace std;
