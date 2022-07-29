3c6d2bfcc28fcd641e6f05e268834e40eb64e442
hirijashree <108250556+hirijashree@users.noreply.github.com>
2022-07-29 09:21:06 +0530

    Add files via upload

---
+++ 10a/10a.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+	
+cin>>argv[0];
+if(argc>0)
+}	     cout<<"overflow error"<<endl;
+}
+else{
+	        cout<<"underflow error"<<endl;
+}
+
+
+
+
+	try{
+       
+	if(argc>1)
+		       {
+		          cout<<argv[2]<<endl;
+			  cout<<argv[3]<<endl;
+			    
+						    }
+	else 
+	{
+		throw ("Insufficient arguments. Enter atleast 2 or more strings" );
+	}}
+	catch (int i)
+	{
+		cout<<"exception caught \n";
+	}
+	
+return 0;
+	       //char *args= {argv[0],argv[1]};
+		  //return 0;
+}
